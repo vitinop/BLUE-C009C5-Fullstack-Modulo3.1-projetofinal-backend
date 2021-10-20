02f0691945d00e8f7045cd3ceb5742be8ed6c8b4
Victor Luz <85527775+vitinop@users.noreply.github.com>
2021-10-20 20:01:10 -0300

    updates

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ Conn(db_url, db_user, db_pass, db_data);
 
 app.use("/tarefas", tarefasRoutes);
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
+// const port = process.env.PORT || 3001;
+
+app.listen(process.env.PORT || 3001, () => {
   console.info(`O app está rodando na porta http://localhost:${port}/`);
 });
